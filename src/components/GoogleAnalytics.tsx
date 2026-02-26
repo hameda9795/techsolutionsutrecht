a873f7c68036b0d9790f8bb251b33f6d43454072
hameda9795 <hameda9795@gmail.com>
2026-02-26 12:46:46 +0100

    Update Google Analytics to G-NBGY87SYXL

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@
 
 import Script from "next/script";
 
-const GA_MEASUREMENT_ID = "G-6L56YD1RP4";
+const GA_MEASUREMENT_ID = "G-NBGY87SYXL";
 
 export default function GoogleAnalytics() {
   return (
